483822792ba8cbbe42da3a92b0d7c71d47233c5c
mosetf <stanlym315@gmail.com>
2024-01-26 19:20:36 +0300

    add docs

--- twosum.py
+++ twosum.py
@@ -1,5 +1,19 @@
 class solution:
-    def twosum(self,nums, target):
+    """
+    Class to find the indices of two numbers in a list that add up to a target value.
+    """
+
+    def twosum(self, nums, target):
+        """
+        Finds the indices of two numbers in the given list that add up to the target value.
+
+        Args:
+            nums (List[int]): The list of numbers.
+            target (int): The target value.
+
+        Returns:
+            List[int]: The indices of the two numbers that add up to the target value.
+        """
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
